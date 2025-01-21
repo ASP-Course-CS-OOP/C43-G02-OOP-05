9171a5a39283d3559325c1d56aca60852de0e3be
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-22 00:49:47 +0200

    Second Project

---
+++ Assignment/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Maths
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Subtract(int a, int b) => a - b;
+        public static int Multiply(int a, int b) => a * b;
+        public static int Divide(int a, int b) => b != 0 ? a / b : throw new DivideByZeroException("Division by zero is not allowed.");
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -35,7 +35,11 @@
 
             #region Second Project
 
-
+            Console.WriteLine("\n=== Second Project ===");
+            Console.WriteLine($"Add: { Maths.Add(10, 5)}");
+            Console.WriteLine($"Subtract: {Maths.Subtract(10, 5)}");
+            Console.WriteLine($"Multiply: {Maths.Multiply(10, 5)}");
+            Console.WriteLine($"Divide: { Maths.Divide(10, 5)}");
 
             #endregion
         }
